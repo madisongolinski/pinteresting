d39930518b4d91c03a623424f0828f28f8b38a4f
madison <madisongolinski@gmail.com>
2014-11-28 13:27:26 -0300

    troubleshooting descrption

--- db/migrate/20141127165513_add_description_to_pins.rb
+++
@@ -1,5 +0,0 @@
-class AddAttachmentImageToPins< ActiveRecord::Migration
-  def change
-    add_column :pins, :description, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141128000726) do
+ActiveRecord::Schema.define(version: 20141128162639) do
 
   create_table "pins", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
-    t.string   "description"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
