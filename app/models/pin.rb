8c8e9b7b511eb81f88ae25ee2ffe7366052a26d3
madison <madisongolinski@gmail.com>
2014-11-28 11:54:56 -0300

    images uploaded

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,12 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+	
+	def fix_image_url(type)
+		type == :orginal unless !type.nil?
+		matches = image.url(type).to_s.match(/%(.*)/)
+		first = matches[1] # this is the first () group
+		string_to_return = image.url(type).to_s.sub! '%'+first, ''
+		string_to_return
+	end
 end
